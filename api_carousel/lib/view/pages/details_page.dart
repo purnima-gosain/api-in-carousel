0b5e0ebfd8af01fdce634e0c2d55770d8b4c0444
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-16 15:34:18 +0545

    style:extracted widget and added padding

--- api_carousel/lib/view/pages/details_page.dart
+++ api_carousel/lib/view/pages/details_page.dart
@@ -41,66 +41,100 @@ class _DetailsPageState extends State<DetailsPage> {
           child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CarouselSlider.builder(
-              itemCount: widget.image!.length,
-              itemBuilder: (context, index, realIndex) {
-                final urlImage = widget.image![index];
-                return Image.network(urlImage);
-              },
-              options: CarouselOptions(
-                  autoPlay: false,
-                  enableInfiniteScroll: false,
-                  enlargeCenterPage: true,
-                  viewportFraction: 1,
-                  autoPlayInterval: const Duration(seconds: 2),
-                  onPageChanged: (index, reason) =>
-                      setState(() => _currentPosition = index))),
-          Center(
-            child: DotsIndicator(
-              dotsCount: widget.image!.length,
-              position: _currentPosition.toDouble(),
-              decorator: DotsDecorator(
-                  size: const Size(19, 5),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0)),
-                  activeSize: const Size(19, 5),
-                  activeShape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0))),
+          slider(),
+          dotsIndicator(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _title(widget: widget),
+                const SizedBox(height: 10),
+                price(),
+                const SizedBox(height: 10),
+                _details(
+                    widget: widget,
+                    description: widget.ratings.toString(),
+                    title: "Ratings: "),
+                const SizedBox(height: 10),
+                _details(
+                    widget: widget,
+                    description: " ${widget.discount.toString()} %",
+                    title: "Discount Percent: "),
+                const SizedBox(height: 10),
+                _details(
+                    widget: widget,
+                    description: widget.stock.toString(),
+                    title: "Stock: "),
+                const SizedBox(height: 10),
+                _details(
+                  widget: widget,
+                  title: "Description: ",
+                  description: widget.description ?? "",
+                ),
+              ],
             ),
-          ),
-          Text(
-            widget.title ?? "",
-            textAlign: TextAlign.left,
-            style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
-          ),
-          const SizedBox(height: 10),
-          Text("Price: \$ ${widget.price.toString()}",
-              textAlign: TextAlign.left,
-              style: const TextStyle(
-                  fontWeight: FontWeight.w700, color: Colors.indigo)),
-          const SizedBox(height: 10),
-          _details(
-              widget: widget,
-              description: widget.ratings.toString(),
-              title: "Ratings: "),
-          const SizedBox(height: 10),
-          _details(
-              widget: widget,
-              description: " ${widget.discount.toString()} %",
-              title: "Discount Percent: "),
-          const SizedBox(height: 10),
-          _details(
-              widget: widget,
-              description: widget.stock.toString(),
-              title: "Stock: "),
-          const SizedBox(height: 10),
-          _details(
-            widget: widget,
-            title: "Description: ",
-            description: widget.description ?? "",
           ),
         ],
       )),
+    );
+  }
+
+  Text price() {
+    return Text("Price: \$ ${widget.price.toString()}",
+        textAlign: TextAlign.left,
+        style:
+            const TextStyle(fontWeight: FontWeight.w700, color: Colors.indigo));
+  }
+
+  Center dotsIndicator() {
+    return Center(
+      child: DotsIndicator(
+        dotsCount: widget.image!.length,
+        position: _currentPosition.toDouble(),
+        decorator: DotsDecorator(
+            size: const Size(19, 5),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0)),
+            activeSize: const Size(19, 5),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0))),
+      ),
+    );
+  }
+
+  CarouselSlider slider() {
+    return CarouselSlider.builder(
+        itemCount: widget.image!.length,
+        itemBuilder: (context, index, realIndex) {
+          final urlImage = widget.image![index];
+          return Image.network(urlImage);
+        },
+        options: CarouselOptions(
+            autoPlay: true,
+            enableInfiniteScroll: false,
+            enlargeCenterPage: true,
+            viewportFraction: 1,
+            autoPlayInterval: const Duration(seconds: 2),
+            onPageChanged: (index, reason) =>
+                setState(() => _currentPosition = index)));
+  }
+}
+
+class _title extends StatelessWidget {
+  const _title({
+    Key? key,
+    required this.widget,
+  }) : super(key: key);
+
+  final DetailsPage widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      widget.title ?? "",
+      textAlign: TextAlign.left,
+      style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
     );
   }
 }

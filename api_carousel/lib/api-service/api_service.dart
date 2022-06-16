f783b5db7dd1b1524816ac888d827567c1946f15
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-16 12:40:44 +0545

    added api service

--- api_carousel/lib/api-service/api_service.dart
+++ api_carousel/lib/api-service/api_service.dart
@@ -1,1 +1,18 @@
+import 'package:dio/dio.dart';
 
+import '../model/model.dart';
+
+List<Products>? productModel;
+
+Future<Laptop> fetchData() async {
+  final Dio dio = Dio();
+  final response =
+      await dio.get("https://dummyjson.com/products/search?q=Laptop");
+  if (response.statusCode == 200) {
+    var value = Laptop.fromJson(response.data);
+    productModel = value.product;
+    return value;
+  } else {
+    throw Exception("Unable to fetch data");
+  }
+}

f2d4296eaf033efec9197e1ecf27be997fe11ff9
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-16 12:54:25 +0545

    added title in appbar

--- api_carousel/lib/view/pages/home_page.dart
+++ api_carousel/lib/view/pages/home_page.dart
@@ -11,7 +11,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Laptops"),
+      ),
       body: Container(),
     );
   }
